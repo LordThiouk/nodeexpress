237739c6783321d4a23688f03030aea7a3a2ed6d
LordThiouk <76528706+LordThiouk@users.noreply.github.com>
2023-09-26 22:28:28 +0000

    nodebackend

---
+++ .history/index_20230925222120.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+      },
+      {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+      },
+      {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+      }
+]
+
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => {
+      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+      return note.id === id
+    })
+    console.log(note)
+    response.json(note)
+  })
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
